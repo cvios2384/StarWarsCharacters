bbaa158529d421c599be22256299ea58407082d5
cvios2384 <chdev5102@gmail.com>
2022-11-11 16:58:07 -0800

    Initial Commit

---
+++ StarWarsCharacters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWarsCharacters
+//
+//  Created by user228909 on 11/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
